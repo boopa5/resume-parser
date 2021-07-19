dca974e0d5c6657d99dcb4a7aa474c49f72e9d9a
Richard Gao <richardgao@Richards-MacBook-Pro.local>
2021-07-19 00:22:21 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='resume_yoe',
+      version='1.0',
+      description='Testing installation of Package',
+      author='Richard Gao',
+      author_email='rgao.2003@gmail.com',
+      license='MIT',
+      packages=['resume_yoe'],
+      zip_safe=False)
